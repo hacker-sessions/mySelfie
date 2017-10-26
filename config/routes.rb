d4002a15254646d25a272b24cb711ae3a9a95fbf
Alberto Rocha <betogrun@gmail.com>
2017-10-26 21:38:45 -0200

    Setup project to handle vue sample component

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :pages
+  root to: 'pages#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
